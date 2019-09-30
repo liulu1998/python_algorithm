b84583b279fb42610ec32757b091bc2bd7276320
liulu1998 <1274272851@qq.com>
2019-09-30 15:39:36 +0800

    新增0-1背包

--- chapter2/0_1背包.py
+++ chapter2/0_1背包.py
@@ -1,19 +1,52 @@
-from numpy import array, ndarray
+import numpy as np
 from numba import jit, njit
 from typing import List
 
-def knapsack(w: List[int], v: List[int], c: float, p: ndarray, head: list[int]) -> float:
+
+def knapsack(v: List[int], w: List[int], c: int, m: np.ndarray) -> None:
     n = len(v) - 1
-    head[n+1] = 0
-    p[0][0] = 0
-    p[0][1] = 0
-    left, right, _next = 0, 0, 1
-    head[n] = 1
-    for i in range(n, 0, -1):
-        k = left
-        for j in range(left, right+1):
-            if p[j][0] + w[i] > c:
-                break
-            y = p[j][0] + w[i]
-            m = p[j][1] + v[i]
-        
+    j_max = min(w[n]-1, c)
+    for j in range(0, j_max+1):
+        m[n][j] = 0
+    for j in range(w[n], c+1):
+        m[n][j] = v[n]
+
+    for i in range(n-1, 0, -1):
+        j_max = min(w[i]-1, c)
+        for j in range(0, j_max+1):
+            m[i][j] = m[i+1][j]
+        for j in range(w[i], c+1):
+            m[i][j] = max(m[i+1][j], m[i+1][j-w[i]] + v[i])
+    
+    m[1][c] = m[2][c]
+    if c>= w[1]:
+        m[1][c] = max(m[1][c], m[2][c-w[1]] + v[1])
+
+
+def traceback(m: np.ndarray, w: List[int], c: int, x: List[int]) -> None:
+    n = len(w) - 1
+    for i in range(0, n):
+        if m[i][c] == m[i+1][c]:
+            x[i] = 0
+        else:
+            x[i] = 1
+            c -= w[i]
+    x[n] = 1 if m[n][c] > 0 else 0
+
+
+if __name__ == "__main__":
+    
+    # 物品价值
+    v = np.array([8, 3, 2, 11, 6])
+    # 物品重量
+    w = np.array([4, 2, 2, 5, 7])
+    # 背包容量
+    c = np.int32(10)
+
+    m = np.zeros((len(v), c+1), dtype=np.int32)
+    knapsack(v, w, c, m)
+
+    print(m)
+    x = np.zeros((len(v),), dtype=np.int32)
+    traceback(m, w, c, x)
+    print(x)

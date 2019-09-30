b86a86e2aa415f83b630d0b1f6a7d29118283650
liulu1998 <1274272851@qq.com>
2019-09-30 13:10:24 +0800

    修改了多边形剖分

--- chapter2/多边形最优三角剖分.py
+++ chapter2/多边形最优三角剖分.py
@@ -6,7 +6,7 @@ def min_weight_triangulation(n: int, t: np.ndarray, s: np.ndarray, w) -> None:
     for r in range(2, n+1):
         for i in range(1, n-r+2):
             j = i + r - 1
-            print(f"i{i} j{j}")
+            # print(f"i{i} j{j}")
             t[i][j] = t[i+1][j] + w(i-1, i, j)
             s[i][j] = i
             
@@ -15,6 +15,7 @@ def min_weight_triangulation(n: int, t: np.ndarray, s: np.ndarray, w) -> None:
                 if u < t[i][j]:
                     t[i][j] = u
                     s[i][j] = k
+
 
 def traceback(s: np.ndarray, i: int, j: int) -> None:
     if i == j:
@@ -34,21 +35,24 @@ def w(a: int, b: int, c: int) -> int:
 
 if __name__ == "__main__":
     weight = [
-        [0,2,2,3,1,4],
-        [2,0,1,5,2,3],
-        [2,1,0,2,1,4],
-        [3,5,2,0,6,2],
-        [1,2,1,6,0,1],
-        [4,3,4,2,1,0]
+        [0,2,2,3,1,4, 0],
+        [2,0,1,5,2,3, 0],
+        [2,1,0,2,1,4, 0],
+        [3,5,2,0,6,2, 0],
+        [1,2,1,6,0,1, 0],
+        [4,3,4,2,1,0, 0]
     ]
 
     weight = np.array(weight, dtype=np.int32)
-    print(weight)
+    # print(weight)
     
     t = np.zeros((weight.shape[0]+1, weight.shape[1]+1), dtype=np.int32)
     s = np.zeros((weight.shape[0]+1, weight.shape[1]+1), dtype=np.int8)
 
-    print(f"t: {t.shape}   s:{s.shape}")
+    # print(f"weight: {weight.shape}")
+    # print(f"t: {t.shape}   s:{s.shape}")
 
     min_weight_triangulation(weight.shape[0], t, s, w)
-    print(t, s, sep="\n\n")
+    # print(t, s, sep="\n\n")
+
+    traceback(s, 1, weight.shape[0]-1)

a529924e057bd962c03b859d4058ef7eb5d83624
liulu1998 <1274272851@qq.com>
2019-10-05 15:16:47 +0800

    add pythonic quickSort

--- chapter1/quick_sort.py
+++ chapter1/quick_sort.py
@@ -2,6 +2,21 @@ import numpy as np
 from typing import List
 
 
+def quick_sort_pythonic(array: List[int]) -> None:
+
+    if len(array) <= 1:
+        return array
+
+    x = array.pop()
+    lower, greater = [], []
+    for a in array:
+        if a > x:
+            greater.append(a)
+        else:
+            lower.append(a)
+    return quick_sort_pythonic(lower) + [x] + quick_sort_pythonic(greater)
+
+
 def quick_sort(a: List[int], p: int, r: int) -> None:
     if p < r:
         q = partition(a, p, r)
@@ -17,7 +32,7 @@ def partition(a: List[int], p: int, r: int) -> int:
     while low < high:
         while low < high and a[high] >= x:
             high -= 1
-        
+
         if low < high:
             a[low] = a[high]
             low += 1
@@ -35,6 +50,14 @@ def partition(a: List[int], p: int, r: int) -> int:
 
 if __name__ == "__main__":
     a = np.random.randint(30, size=10)
+    b = list(a)
+
+    # 原始数组
     print(a)
+
     quick_sort(a, 0, len(a)-1)
     print(a)
+
+    # 返回 b 的副本
+    b = quick_sort_pythonic(b)
+    print(b)

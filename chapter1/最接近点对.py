21088b3f971f3ffbd6a710fbd047d0594d44d41a
liulu1998 <1274272851@qq.com>
2019-10-11 20:37:21 +0800

    修改最近点对

--- chapter1/最接近点对.py
+++ chapter1/最接近点对.py
@@ -1,163 +1,73 @@
-from math import sqrt
 import numpy as np
 from typing import List
-import matplotlib.pyplot as plt
-# from merge_sort_alter import merge
 
 
-class Point(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"x:{self.x} y:{self.y}"
-
-    def __eq__(self, other):
-        """等于方法
-        """
-        return self.x == other.x
-
-    def __lt__(self, other):
-        """小于方法
-        """
-        return self.x < other.x
-    
-    def __le__(self, other):
-        return self.x <= other.x
-
-class Point1(Point):
-    """
-        依 x坐标排序的点
-    """
-    def __init__(self, x, y, id):
-        super().__init__(x, y)
-        self.id = id
-
-    def __str__(self):
-        return super().__str__() + f" id:{self.id}"
-
-
-class Point2(Point):
-    """
-        依 y坐标排序的点
-    """
-    def __init__(self, x, y, p):
-        super().__init__(x, y)
-        self.p = p
-
-    def __str__(self):
-        return super().__str__() + f" p:{self.p}"
-
-    def __lt__(self, other):
-        return self.y < other.y
-
-    def __eq__(self, other):
-        return self.y == other.y
-    
-    def __le__(self, other):
-        return self.y <= other.y
- 
 class Pair(object):
-    """
-        输出的平面点对
-    """
-    def __init__(self, a: Point1, b: Point1, dist: float):
-        self.a = a
-        self.b = b
-        self.dist = dist
+    def __init__(self, p1, p2, dist=None):
+        self.p1 = p1
+        self.p2 = p2
+        self.dist = dist if dist is not None else self.distance(p1, p2)
 
+    @staticmethod
+    def distance(a, b) -> float:
+        return np.sqrt( (a[0]-b[0])**2 + (a[1] - b[1])**2 )
+
+    def __lt__(self, other):
+        return self.dist < other.dist
+    
     def __str__(self):
-        return self.a.__str__() +" and "+ self.b.__str__() + f" | dist:{self.dist}"
+        return f"-----------\n({self.p1[0]}, {self.p1[1]}) and ({self.p2[0]}, {self.p2[1]})\ndist: {self.dist}"
+    
 
 
-def dist(u: Point, v: Point) -> float:
-    """
-    计算两点欧式距离
+def cpair2(x: np.ndarray) -> Pair:
 
-    :param u: Point, 点1
-    :param v: Point, 点2
-    :return float, 两点欧氏距离
-    """
-    x = u.x - v.x
-    y = u.y - v.y
-    return sqrt(x**2 + y**2)
+    # 三点
+    if x.shape[0] == 3:
+            d1 = Pair.distance(x[0], x[1])
+            d2 = Pair.distance(x[1], x[2])
+            d3 = Pair.distance(x[0], x[2])
+            if d1 <= d2 and d1 <= d3:
+                return Pair(x[0], x[1], d1)
+            elif d2 <= d3:
+                return Pair(x[1], x[2], d2)
+            else:
+                return Pair(x[0], x[2], d3)
+    # 两点
+    if x.shape[0] == 2:
+        return Pair(x[0], x[1])
+    # 一点
+    if x.shape[0] == 1:
+        return Pair(None, None, np.float("inf"))
 
+    # 依 x坐标排序
+    x = x[np.argsort(x[:, 0])]
+    # 依 y坐标排序
+    y = x[np.argsort(x[:, 1])]
 
-def cpair2(x: List[Point1]) -> Pair:
-    """
-    二维平面的最接近点对
-    """
-    if len(x) < 2:
-        return None
+    # x 坐标中位数点的索引
+    mid = x.shape[0] // 2
 
-    # 依 x坐标排序, in-place 操作
-    x.sort()
-    y = [ Point2(p.x, p.y, i) for (i, p) in enumerate(x) ]
-    # 依 y坐标排序, in-place 操作
-    y.sort()
-    z = list(range(len(y)*2))
-    return closest_pair(x, y, z, 0, len(x)-1)
+    # 左, 右部分最近点对
+    pair1: Pair = cpair2(x[0: mid])
+    pair2: Pair = cpair2(x[mid: x.shape[0]])
 
+    best = pair1 if pair1 < pair2 else pair2
 
-def closest_pair(x: List[Point1], y: List[Point2], z: List[Point2], l: int, r: int) -> Pair:
-    # 两点的情况
-    if r-l == 1:
-        return Pair(x[l], x[r], dist(x[l], x[r]))
-    # 三点的情况
-    if r-l == 2:
-        d1 = dist(x[l], x[l+1])
-        d2 = dist(x[l+1], x[r])
-        d3 = dist(x[r], x[l])
+    # d矩形条内的点
+    z = y[abs(y[:, 0] - x[mid][0]) < best.dist]
 
-        if d1 <= d2 and d1 <= d3:
-            return Pair(x[l], x[l+1], d1)
-        elif d2 <= d3:
-            return Pair(x[l+1], x[r], d2)
-        else:
-            return Pair(x[l], x[r], d3)
-    # 多于三点, 分治法
-    # l, r 中点
-    m = (l+r) // 2
-    f = l
-    g = m+1
-    for i in range(l, r+1):
-        if y[i].p > m:
-            z[g] = y[i]
-            g += 1
-        else:
-            z[f] = y[i]
-            f += 1
-
-    # 左右两部分递归求解
-    best: Pair = closest_pair(x, z, y, l, m)  # 左半部分
-    right: Pair = closest_pair(x, z, y, m+1, r)  # 右半部分
-
-    if right.dist < best.dist:
-        best = right
-
-    # 重构数组 y
-    # merge(z, y ,l, m, r)
-
-    # d 矩形条内的点 置于 z 中
-    k = l
-    for i in range(l, r+1):
-        if abs(x[m].x - y[i].x) < best.dist:
-            z[k] = y[i]
-            k += 1
-
-    # 搜索 z[l: k]
-    for i in range(l, k):
-        for j in range(i+1, k):
-            if z[j].y - z[i].y < best.dist:
-                dp = dist(z[i], z[j])
-                if dp < best.dist:
-                    best = Pair(x[z[i].p], x[z[j].p], dp)
+    for i in range(z.shape[0]):
+         for j in range(i+1, z.shape[0]):
+             if z[j][1] - z[i][1] < best.dist:
+                 dp = Pair.distance(z[i], z[j])
+                 if dp < best.dist:
+                     best = Pair(z[i], z[j], dp)
     return best
 
 
 if __name__ == "__main__":
-    x = [
+    S = [
         (0, 1), (3, 2),
         (4, 3), (5, 1),
         (2, 1), (1, 2),
@@ -166,6 +76,7 @@ if __name__ == "__main__":
         (9, 0), (6, 4)
     ]
 
-    x = [Point1(p[0], p[1], i) for i, p in enumerate(x)]
-    pair = cpair2(x) # print(pair)
+    S = np.array(S, dtype=np.float32)
+
+    pair = cpair2(S)
     print(pair)
